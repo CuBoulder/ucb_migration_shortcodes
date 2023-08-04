d183bbb233207709c222fb3a559564633eb34153
Jacob Korf <korfj921921@gmail.com>
2023-08-04 08:11:21 -0600

    countdown js

--- js/expand-content.js
+++ js/expand-content.js
@@ -107,4 +107,32 @@ document.addEventListener('DOMContentLoaded', function() {
   
       animateValue();
     });
+
+    // Countdown
+    var counters = document.querySelectorAll('.cu-countdown');
+    counters.forEach(setInterval(function(counter) {
+    
+      var countDownDate = new Date(counter.innerText).getTime();
+      // Get today's date and time
+      var now = new Date().getTime();
+    
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+    
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+      // Display the result in the element with id="demo"
+      document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+      + minutes + "m " + seconds + "s ";
+    
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "DONE";
+      }
+    }, 1000));
   });
