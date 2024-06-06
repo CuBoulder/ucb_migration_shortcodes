159cacd8052d83c6e9c42e4327295efe4c07a23a
Joshua Nicholson <94021017+jnicholCU@users.noreply.github.com>
2024-06-06 12:57:42 -0600

    countup update
    Updated countup to work properly with decimals, times, and other modifiers like monetary signs.
    Using the counterup-2 code which is a newer version of the currently used code on both D7 and D10

--- js/expand-content.js
+++ js/expand-content.js
@@ -81,33 +81,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 
-  // Countup
-  var counters = document.querySelectorAll('.counter');
-  counters.forEach(function (counter) {
-    var delay = parseInt(counter.getAttribute('data-delay'));
-    var time = parseInt(counter.getAttribute('data-time'));
-
-    function animateValue() {
-      var start = 0;
-      var end = parseInt(counter.innerText);
-      var duration = time / delay;
-      var range = end - start;
-      var current = start;
-      var increment = end > start ? 1 : -1;
-      var stepTime = Math.abs(Math.floor(duration / range));
-
-      var timer = setInterval(function () {
-        current += increment;
-        counter.innerText = current;
-        if (current === end) {
-          clearInterval(timer);
-        }
-      }, stepTime);
-    }
-
-    animateValue();
-  });
-
   // Countdown
   var counters = document.querySelectorAll('.cu-countdown');
   for (const counter of counters) {
